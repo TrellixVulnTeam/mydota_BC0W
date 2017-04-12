e1246cafbd23be76a945fa6626a34a6d056308dd
Letsopappaaa <peteroravecz94@gmail.com>
2017-04-13 00:44:52 +0200

    4.13

--- env/dota_site/main/urls.py
+++ env/dota_site/main/urls.py
@@ -1,10 +1,18 @@
 from django.conf.urls import url
 from django.contrib import admin
-from .views import match_detail_view, main_view
+from .views import (
+    match_detail_view,
+    main_view,
+    player_summary_view,
+    search_view
+    )
 
 urlpatterns = [
     url(r'^$', main_view, name='home'),
     url(r'^matches/(?P<match_id>\d{10})', match_detail_view, name='match_detail'),
+    url(r'^player/(?P<account_id>\d*)', player_summary_view, name='player_summary'),
+    url(r'^search/(?P<search_id>\d*)', search_view, name='search_view'),
+
 
 
     
--- env/dota_site/main/views.py
+++ env/dota_site/main/views.py
@@ -6,6 +6,7 @@ import json
 import jsbeautifier
 from json2html import *
 import os
+from datetime import timedelta
 
 api = dota2api.Initialise()
 
@@ -28,18 +29,95 @@ def main_view(request):
         return render(request, 'main.html')
 
 
-def match_detail_view(request, match_id=None):
-
+def player_summary_view(request, account_id):
 
     query = request.GET.get("q")
     if query:
-        match_id = query
-    match = api.get_match_details(match_id=match_id)
+        return redirect('main:match_detail', match_id = query)
+
+    player_history = api.get_match_history(account_id=account_id, matches_requested=2)
+    player_history = player_history["matches"]
+    player_history_ = json2html.convert(json=player_history)
+    heroes = api.get_heroes()
+    heroes_ = json2html.convert(json=heroes)
+    heroes = heroes["heroes"]
+    player_sum = api.get_player_summaries(steamids=76561198086198097)
+
+    for current_match in player_history:
+        match = api.get_match_details(match_id=current_match["match_id"])
+        current_match["duration"] = match["duration"]
+        current_match["duration"] = timedelta(seconds=current_match["duration"])
+
+
+        # Handling player picture
+        def player_pic():
+            global x
+            x=0
+            while x<10:
+                if int(current_match["players"][x]["account_id"]) == int(account_id):
+                    for hero in heroes:
+                        if hero["id"] == current_match["players"][x]['hero_id']:
+                            return hero["url_large_portrait"]
+                x+=1
+        current_match["player_pic"] = player_pic()
+        current_match["player_kills"] = match["players"][x]["kills"]
+        current_match["player_deaths"] = match["players"][x]["deaths"]
+        current_match["player_assists"] = match["players"][x]["assists"]
+        current_match["game_mode_name"] = match["game_mode_name"]
+
+        # Handling Result win/loss
+        # rad_win=0
+        # rad_member=0
+        # if match["radiant_win"] == True:
+        #     rad_win = 1
+        # else:
+        #     rad_win = 0
+        # i=0
+        # while i<5:
+        #     if match["players"][i]["account_id"] == str(account_id):
+        #         rad_member= 1
+        #         i=5
+        #     i+=1
+        # print(rad_member)
+        # print(rad_win)
+        # current_match["victory"] = rad_win+rad_member
+        # if rad_member + rad_win in (2, 0):
+        #     current_match["victory"] = "Won match"
+        # else:
+        #     current_match["victory"] = "Lost match"
 
 
 
+    context={
+        "player_history" : player_history,
+        "player_history_" : player_history_,
+        "player_sum": player_sum,
+    }
 
-    match_ = json2html.convert(json=match)
+    return render(request, "player_summary.html", context)
+
+def search_view(request, search_id):
+
+
+    if api.get_match_details(match_id=search_id):
+        print('helo')
+    else:
+        api.get_match_history(account_id=search_id, matches_requested=1)
+        print('íyello')
+    return HttpResponse("hu")
+    # match_id = query
+    # player_history = api.get_match_history(account_id=account_id, matches_requested=2)
+    # player_history = player_history["matches"]
+    # match = api.get_match_details(match_id=match_id)
+    # match_ = json2html.convert(json=match)
+
+
+
+def match_detail_view(request, match_id=None):
+
+    query = request.GET.get("q")
+    if query:
+        return redirect('main:search_view', search_id=query)
 
     items = api.get_game_items()
     items_ = json2html.convert(json=items)
@@ -47,10 +125,12 @@ def match_detail_view(request, match_id=None):
     heroes = api.get_heroes()
     heroes_ = json2html.convert(json=heroes)
 
+    match = api.get_match_details(match_id=match_id)
+    match_ = json2html.convert(json=match)
+
     with open(load_json_file("abilities.json")) as abilities_json:
         abilities = json.load(abilities_json)
     abilities = abilities["abilities"]
-
 
 
     if match["radiant_win"] == True:
@@ -63,6 +143,15 @@ def match_detail_view(request, match_id=None):
     rad_heroes = players[0:5]
     dire_heroes = players[5:10]
     builds_col_num = range(1, 26)
+    match["duration"] = timedelta(seconds=match["duration"])
+
+
+    for player in players:
+        player["stead_id"] = player["account_id"] + 76561197960265728
+        player_sum = api.get_player_summaries(steamids=player["stead_id"])
+        player["name"] = player_sum["players"][0]["personaname"]
+        player["prof_pic"] = player_sum["players"][0]["avatarmedium"]
+
 
     # Handling item and hero image URLs
 
